ff5b3546d40764c284c7fd681da955fcfaf8ff1b
drbh <david.richard.holtz@gmail.com>
2019-12-18 18:33:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# gocar
+
+kinda like cargo for Rust - but a cheap knockoff for Golang. 
+
+
+## Commands
+
+```bash
+gocar new [project-name]
+```
+
+```bash
+gocar run
+```
+
+
+## Whats created
+
+```
+$ tree .
+.
+├── gocar.toml
+├── README.md
+└── src
+    └── main.go
+```
+
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/BurntSushi/toml"
+	"github.com/urfave/cli"
+)
+
+type TomlConfig struct {
+	Package packageS          `toml:"package"`
+	Bin     binary            `toml:"bin"`
+	Deps    map[string]string `toml:"deps"`
+	Gets    map[string]string `toml:"gets"`
+}
+
+type packageS struct {
+	Name    string
+	Version string
+	Authors []string
+}
+
+type binary struct {
+	Loc string
+}
+
+func createDirectory(directoryPath string) {
+	//choose your permissions well
+	pathErr := os.MkdirAll(directoryPath, 0777)
+
+	//check if you need to panic, fallback or report
+	if pathErr != nil {
+		fmt.Println(pathErr)
+	}
+}
+func createFiles(filePath, content string) {
+	f, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = f.WriteString(content)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "greet"
+	app.Usage = "fight the loneliness!"
+
+	app.Action = func(c *cli.Context) error {
+
+		switch c.Args().Get(0) {
+		case "new":
+
+			name := c.Args().Get(1)
+			createDirectory("./" + name + "/src")
+			createFiles("./"+name+"/src/main.go", `package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world")
+}
+`)
+			createFiles("./"+name+"/gocar.toml", `[package]
+name = "`+name+`"
+version = "0.1.0"
+authors = [""]
+
+[bin]
+loc = "src/main.go"
+
+[deps]
+
+[gets]
+`)
+			createFiles("./"+name+"/.gitignore", ``)
+
+			createFiles("./"+name+"/README.md", `# `+name+`
+
+**generated with gocar**
+`)
+			// fmt.Println("one")
+		case "run":
+
+			var config TomlConfig
+			if _, err := toml.DecodeFile("gocar.toml", &config); err != nil {
+				fmt.Println(err)
+				return nil
+			}
+			out, err := exec.Command("go", "run", "./"+config.Bin.Loc).Output()
+			if err != nil {
+				fmt.Printf("%s", err)
+			}
+			output := string(out[:])
+			fmt.Println(output)
+
+			// fmt.Println(config)
+
+		case "add":
+
+			fmt.Println("should add line to .toml")
+
+		default:
+			fmt.Println("It's after noon")
+		}
+		return nil
+
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
